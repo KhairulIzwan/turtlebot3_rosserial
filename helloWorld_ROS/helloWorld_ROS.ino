8b24be02081d233c722e5516857b12d308f48803
Wansnap <wansnap@gmail.com>
2019-04-23 10:34:04 +0800

    New

--- helloWorld_ROS/helloWorld_ROS.ino
+++ helloWorld_ROS/helloWorld_ROS.ino
@@ -5,17 +5,13 @@
 
 #include <ros.h>
 #include <std_msgs/String.h>
-#include <std_msgs/Int32.h>
 
 ros::NodeHandle  nh;
 
 std_msgs::String str_msg;
-std_msgs::Int32 int_msg;
 ros::Publisher chatter("chatter", &str_msg);
-ros::Publisher chatter("chatterNo", &int_msg);
 
 char hello[13] = "hello world!";
-int count = 0;
 
 void setup()
 {
@@ -26,11 +22,7 @@ void setup()
 void loop()
 {
   str_msg.data = hello;
-  int_msg.data = count;
   chatter.publish( &str_msg );
-  chatter.publish( &int_msg );
   nh.spinOnce();
   delay(1000);
-
-  ++count;
 }
